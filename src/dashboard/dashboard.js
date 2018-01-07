91335be1e67ae457c4b1bc447a9c238f3987bcab
Unknown <oliveira.ambrozio@gmail.com>
2018-01-07 00:28:45 -0200

    dashBoard +ou-

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/Content';
+import ValueBox from '../common/widget/ValueBox'
+
 
 
 class Dashboard extends Component {
@@ -11,7 +13,12 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title='Dashboard' small='1.0' />
                 <Content>
-                    Dashboard
+                    <ValueBox cols='12 4' color='green' icon='bank'
+                        value='R$ 10' text='Total de Créditos'/>
+                    <ValueBox cols='12 4' color='red' icon='credt-card'
+                        value='R$ 10' text='Total de Debitos'/>
+                    <ValueBox cols='12 4' color='blue' icon='money'
+                        value='R$ 0' text='Valor Consolidado'/>
                 </Content>
             </div>
         );
